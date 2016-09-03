6c8a0f9cccd512b82c36c7cf196ac53587463972
Kátia Nakamura <katiayn@gmail.com>
2016-09-03 19:42:16 +0200

    Import project

---
+++ django_tasks_report/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for django_tasks_report project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tasks_report.settings")
+
+application = Cling(get_wsgi_application())
